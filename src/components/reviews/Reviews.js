e57c83a4512032d334a10d00673fefcf65a03bc3
Erin Ponsonby <erinponsonby@gmail.com>
2020-02-11 15:39:44 -0500

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -7,8 +7,6 @@ class Reviews extends Component {
   renderReviews = () => {
     let associatedReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId )
     // let associatedReviews = this.props.reviews
-    if(associatedReviews.findIndex(review => review.text === "Was great" || review.text === "Was not great") != -1) {
-    }
     return associatedReviews.map(review => <Review key={review.id} review={review} text={review.text} id={review.id} restaurantId={review.restaurantId} delete={this.props.deleteReview}/> )
   }
   render() {
