4f0ce79b31d1df8fb82acb24005781849ee48b45
Dan Sullivan <dansullivan19@gmail.com>
2020-02-13 12:15:59 -0500

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state =  {
+    text: '',
+  }
+
+  handleChange  = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: '',
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input type="text" onChange={(e) => this.handleChange(e)} value={this.state.text}/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 };
 
-export default RestaurantInput;
+export default RestaurantInput
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,21 @@
-
 import cuid from 'cuid';
-export const cuidFn = cuid;
+// export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants( state = { restaurants: [], reviews: [], }, action) {
+    switch (action.type) {
+    case 'ADD_RESTAURANT':
+        const restaurant = {id: cuid(), text: action.payload}
+        return {...state, restaurants: [ ...state.restaurants, restaurant] }
+    case 'DELETE_RESTAURANT':
+        const restaurants = state.restaurants.filter(r => r.id !== action.id);
+        return { ...state, restaurants}
+    case 'ADD_REVIEW':
+        const review = {id: Math.floor(Math.random() * 1000), text: action.review.text, restaurantId: action.review.restaurantId }
+        return {...state, reviews: state.reviews.concat(review)}
+    case 'DELETE_REVIEW':
+        const reviews = state.reviews.filter(r => r.id !== action.id);
+        return { ...state, reviews}
+    default:
+        return state
+    }
 }
