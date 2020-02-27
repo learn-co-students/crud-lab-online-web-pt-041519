76065f330b1a8172fca5898c48bcbd17a358fef3
aparkening <aaron.parkening@gmail.com>
2020-02-27 10:08:08 -0500

    Move button back in li.

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -3,12 +3,10 @@ import React from 'react';
 const Review = props => {
   const { review } = props
   return (
-    <div>
-      <li>
-        {review.text}
-      </li>
+    <li>
+      {review.text}
       <button onClick={() => props.deleteReview(review.id)}> X </button>
-    </div>
+    </li>
   );
 };
 
